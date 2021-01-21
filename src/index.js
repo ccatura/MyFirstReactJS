e8e38431dc2e711949e60e99caa03951cf18195a
Charles Catura <45645258+ccatura@users.noreply.github.com>
2021-01-21 15:28:28 -0500

    nothing really

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import "./styles.css";
+
+const hello = <h1>Hello</h1>;
+
+function App() {
+  var a = 0;
+  a = a + 3;
+  if (a === 4) {
+    return "hi";
+  } else {
+    return hello;
+  }
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
